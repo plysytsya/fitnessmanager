ac8b7df456ea4a566a281daa8e2044b43997f0aa
Pascha <pavlo@dnlab.de>
2023-05-07 00:10:35 +0200

    updates env vars and gitignore

--- fitnessmanager_api/fitnessmanager_api/settings.py
+++ fitnessmanager_api/fitnessmanager_api/settings.py
@@ -102,11 +102,11 @@ WSGI_APPLICATION = "fitnessmanager_api.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fitnessmanager',
-        'USER': 'postgres',
-        'PASSWORD': 'unchained',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
