00b7f7ecbd245e335a2b2ada95b0fbada3e936a0
Pascha <pavlo@dnlab.de>
2023-05-06 23:23:34 +0200

    initial commit

---
+++ fitnessmanager_api/fitnessmanager_api/admin.py
@@ -0,0 +1,63 @@
+from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext as _
+from .models import Customer, Payment
+
+
+class PaymentInline(admin.TabularInline):
+    model = Payment
+    extra = 0
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = [PaymentInline]
+
+    list_display = (
+        "first_name",
+        "last_name",
+        "passport_number",
+        "address",
+        "email",
+        "phone_number",
+        "date_of_birth",
+        "membership_start_date",
+        "notes",
+        "payment_link",
+        "active_membership",
+        "thumbnail",
+    )
+
+    def thumbnail(self, obj):
+        if obj.profile_picture:
+            return format_html(
+                '<img src="{}" width="50" />'.format(obj.profile_picture.url)
+            )
+        else:
+            return ""
+
+    thumbnail.short_description = "Foto"
+
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+
+    def payment_link(self, obj):
+        url = reverse('admin:fitnessmanager_api_payment_changelist')
+        link_text = _('View payments')
+        return format_html('<a href="{}?customer__id={}">{}</a>', url, obj.id, link_text)
+    payment_link.short_description = _('Payments')
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "customer",
+        "date",
+        "amount",
+        "discount_percent",
+        "payment_method",
+        "paid_month",
+        "paid_year",
+    )
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Payment, PaymentAdmin)
---
+++ fitnessmanager_api/fitnessmanager_api/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+# define the choices for the paid_month field
+PAID_MONTH_CHOICES = [(i, str(i)) for i in range(1, 13)]
+
+# define the choices for the paid_year field
+PAID_YEAR_CHOICES = [(i, str(i)) for i in range(2023, 2043)]
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=255, verbose_name=_("Last Name"))
+    date_of_birth = models.DateField(verbose_name=_("Date of Birth"))
+    address = models.CharField(max_length=255, verbose_name=_("Address"))
+    phone_number = models.CharField(max_length=20, verbose_name=_("Phone Number"))
+    email = models.EmailField(max_length=255, verbose_name=_("Email"))
+    registration_date = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Registration Date")
+    )
+    active_membership = models.BooleanField(
+        default=False, verbose_name=_("Active Membership")
+    )
+    membership_start_date = models.DateField(
+        null=True, blank=True, verbose_name=_("Membership Start Date")
+    )
+    membership_end_date = models.DateField(
+        null=True, blank=True, verbose_name=_("Membership End Date")
+    )
+    notes = models.TextField(blank=True, verbose_name=_("Notes"))
+    profile_picture = models.ImageField(
+        upload_to="customer_profile_pictures",
+        null=True,
+        blank=True,
+        verbose_name=_("Profile Picture"),
+    )
+    passport_number = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name=_("Passport Number")
+    )
+    weight = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_("Weight")
+    )
+    height = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True, verbose_name=_("Height")
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = _("Customer")
+        verbose_name_plural = _("Customers")
+
+
+class Payment(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="payments", verbose_name=_("Customer")
+    )
+    date = models.DateField(null=True, blank=True, verbose_name=_("Payment Date"))
+    amount = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        verbose_name=_("Amount Paid"),
+    )
+    discount_percent = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        verbose_name=_("Discount Percent"),
+    )
+    payment_method = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('Payment Method'))
+    paid_month = models.PositiveIntegerField(verbose_name=_('Paid Month'), choices=PAID_MONTH_CHOICES)
+    paid_year = models.PositiveIntegerField(verbose_name=_('Paid Year'), choices=PAID_YEAR_CHOICES)
+
+    def __str__(self):
+        return f"Pago {self.customer.first_name} {self.customer.last_name} - {self.date}"
+
+    class Meta:
+        verbose_name = _("Payment")
+        verbose_name_plural = _("Payments")
