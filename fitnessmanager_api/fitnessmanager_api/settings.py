e98f40703b8d628fbbc8933ebd190e13866baaa1
Pavlo Lysytsya <pavlo@dnlab.de>
2023-05-14 22:36:20 +0200

    wip

--- fitnessmanager_api/fitnessmanager_api/settings.py
+++ fitnessmanager_api/fitnessmanager_api/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=2),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
